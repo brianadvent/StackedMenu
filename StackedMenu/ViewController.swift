f1f851b5beeaab05e044eb6b901c7c861c1de7e2
Training <training@sinatra.fritz.box>
2016-08-14 19:52:10 +0200

    Initial Commit

---
+++ StackedMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackedMenu
+//
+//  Created by Training on 14/08/16.
+//  Copyright © 2016 Training. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
